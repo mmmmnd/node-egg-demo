e183371c68e3cd5d97060d4f4398ac884ac88f97
mmmmnd <handsome.mo@foxmail.com>
2020-12-01 17:57:00 +0800

    菜单权限拆分游客及权限接口

--- server/app/controller/routes.js
+++ server/app/controller/routes.js
@@ -5,7 +5,7 @@
  * @version: 1.0.0
  * @Date: 2020-06-30 19:36:54
  * @LastEditors: 莫卓才
- * @LastEditTime: 2020-12-01 00:11:42
+ * @LastEditTime: 2020-12-01 10:38:28
  */
 
 'use strict';
@@ -15,9 +15,16 @@ class RoutesController extends Controller {
   /**
    * 获取列表
    */
-  async index () {
+  async roles () {
     const admin = await this.ctx.service.admin.list();
-    const routes = await this.ctx.service.routes.index(admin);
+    const routes = await this.ctx.service.routes.roles(admin);
+    await this.ctx.helper.checkData(routes);
+  }
+  /**
+   * 游客权限
+   */
+  async constant () {
+    const routes = await this.ctx.service.routes.constant();
     await this.ctx.helper.checkData(routes);
   }
 }
--- server/app/service/routes.js
+++ server/app/service/routes.js
@@ -5,7 +5,7 @@
  * @version: 1.0.0
  * @Date: 2020-11-28 20:59:29
  * @LastEditors: 莫卓才
- * @LastEditTime: 2020-12-01 00:11:17
+ * @LastEditTime: 2020-12-01 10:38:16
  */
 'use strict';
 
@@ -13,11 +13,25 @@ const { Op } = require('sequelize');
 const Service = require('egg').Service;
 const GetTree = require('../utils/getTree');
 const HttpStatus = require('../utils/httpStatus');
-
 class RoutesService extends Service {
 
-  async index (admin) {
-
+  /**
+   * 角色权限
+   * @param { Array } admin 用户数据
+   */
+  async roles (admin) {
+    const permissionsRoutes = await this.ctx.model.MzcRoutes.findAll({
+      where: {
+        role: { [Op.ne]: 0 },
+      },
+      attributes: { exclude: ['created_at', 'updated_at', 'deleted_at'] },
+    });
+    return { data: GetTree.routesList(permissionsRoutes, [], admin) };
+  }
+  /**
+   * 游客权限
+   */
+  async constant () {
     const constantRoutes = await this.ctx.model.MzcRoutes.findAll({
       where: {
         role: 0,
@@ -25,20 +39,7 @@ class RoutesService extends Service {
       },
       attributes: { exclude: ['created_at', 'updated_at', 'deleted_at'] },
     });
-
-    const permissionsRoutes = await this.ctx.model.MzcRoutes.findAll({
-      where: {
-        role: { [Op.ne]: 0 },
-      },
-      attributes: { exclude: ['created_at', 'updated_at', 'deleted_at'] },
-    });
-
-    return {
-      data: {
-        constantRoutes: GetTree.routesList(constantRoutes),
-        permissionsRoutes: GetTree.routesList(permissionsRoutes, [], admin),
-      },
-    };
+    return { data: GetTree.routesList(constantRoutes) };
   }
 }
 
